dbbc830d5f30bd09637967b6094f26a2096ea57e
Pincle Soto <lpinclesoto@deloitte.com>
2023-11-10 09:56:54 -0600

    changing app launch template

--- modules/app-ltp/main.tf
+++ modules/app-ltp/main.tf
@@ -2,7 +2,7 @@ resource "aws_launch_template" "app_launch_template" {
   name_prefix   = "app_launch_template"
   image_id      = var.app_ami_id
   instance_type = "t2.micro"
-  security_group_names = [ var.PrivateInstanceSG ]
+  #security_group_names = [ var.PrivateInstanceSG ]
   
   iam_instance_profile {
     name = var.ec2_role
